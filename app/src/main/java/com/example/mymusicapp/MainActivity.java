008f8f1da79f9de04426dad044eb7852d13db5a4
luongtx <tranxuanluong1998@gmail.com>
2020-06-19 23:23:30 +0700

    add viewpager, fragment, bind tablayout with viewpager

--- app/src/main/java/com/example/mymusicapp/MainActivity.java
+++ app/src/main/java/com/example/mymusicapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mymusicapp;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -9,11 +10,23 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
     TabLayout tabLayout;
     ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tab_layout);
+        toolbar = findViewById(R.id.toolbar_main);
+        viewPager = findViewById(R.id.view_pager);
+
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new SongsFrag(), "SONGS");
+        pagerAdapter.addFragment(new ArtistsFrag(), "ARTISTS");
+        pagerAdapter.addFragment(new AlbumsFrag(), "ALBUMS");
+        pagerAdapter.addFragment(new PlaylistFrag(), "PLAYLIST");
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/com/example/mymusicapp/MyPagerAdapter.java
+++ app/src/main/java/com/example/mymusicapp/MyPagerAdapter.java
@@ -1,50 +1,44 @@
 package com.example.mymusicapp;
 
-import android.content.Context;
-
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
-class MyPagerAdapter extends FragmentStatePagerAdapter {
-    Context context;
-    int totalTabs;
-    private String[] pageTitles = new String[]{"SONGS", "ARTISTS", "ALBUMS", "PLAYLIST"};
-    public MyPagerAdapter(Context c, FragmentManager fm, int totalTabs) {
+import java.util.ArrayList;
+
+public class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+    private ArrayList<String> list_page_title;
+    private ArrayList<Fragment> list_page_fragment;
+    public MyPagerAdapter(@NonNull FragmentManager fm) {
         super(fm);
-        context = c;
-        this.totalTabs = totalTabs;
+        list_page_title = new ArrayList<>();
+        list_page_fragment = new ArrayList<>();
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        return pageTitles[position];
+        return list_page_title.get(position);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return list_page_fragment.get(position);
     }
 
     @Override
-    public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                SongsFrag songsFrag = new SongsFrag();
-                return songsFrag;
-            case 1:
-                ArtistsFrag artistsFrag = new ArtistsFrag();
-                return artistsFrag;
-            case 2:
-                AlbumsFrag albumsFrag = new AlbumsFrag();
-                return albumsFrag;
-            case 3:
-                PlaylistFrag playlistFrag = new PlaylistFrag();
-                return playlistFrag;
-            default:
-                return null;
-        }
-    }
-    @Override
     public int getCount() {
-        return totalTabs;
+        return list_page_fragment.size();
     }
+
+    public void addFragment(Fragment fragment, String title) {
+        list_page_fragment.add(fragment);
+        list_page_title.add(title);
+    }
+
 }
