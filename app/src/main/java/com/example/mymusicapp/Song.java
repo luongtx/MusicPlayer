b19567c29209fe712596b866f25d8d12784a6dee
luongtx <tranxuanluong1998@gmail.com>
2020-06-20 22:34:08 +0700

    add listview for songs

---
+++ app/src/main/java/com/example/mymusicapp/Song.java
@@ -0,0 +1,47 @@
+package com.example.mymusicapp;
+
+public class Song {
+    private int id;
+    private String title;
+    private String artist;
+    private String album;
+
+    public Song(int id, String title, String artist, String album) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+}
--- app/src/main/java/com/example/mymusicapp/SongsFrag.java
+++ app/src/main/java/com/example/mymusicapp/SongsFrag.java
@@ -2,6 +2,8 @@ package com.example.mymusicapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -24,5 +26,11 @@ public class SongsFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_songs, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
