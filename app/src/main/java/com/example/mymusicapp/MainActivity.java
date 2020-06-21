8632ef5ca83006cf8df5ceaf498c62ebf675591b
luongtx <tranxuanluong1998@gmail.com>
2020-06-21 21:47:40 +0700

    add music service

--- app/src/main/java/com/example/mymusicapp/MainActivity.java
+++ app/src/main/java/com/example/mymusicapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.mymusicapp;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 
 import androidx.appcompat.widget.Toolbar;
@@ -11,10 +16,16 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     TabLayout tabLayout;
     ViewPager viewPager;
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound=false;
+    private ArrayList<Song> song_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tab_layout);
         toolbar = findViewById(R.id.toolbar_main);
         viewPager = findViewById(R.id.view_pager);
-
         setSupportActionBar(toolbar);
         MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
         pagerAdapter.addFragment(new SongsFrag(), "SONGS");
@@ -31,6 +41,34 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter.addFragment(new PlaylistFrag(), "PLAYLIST");
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private ServiceConnection musicConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            //get service
+            musicSrv = binder.getService();
+            //pass list
+            musicSrv.setList(song_list);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(playIntent==null){
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
     }
 
     @Override
